8157cb2fb83deea8d0c60f347aab2a63bfbd8e06
NotTheJerry <quinterodiego208@gmail.com>
2024-04-22 07:01:22 -0600

    Commit inicial ;)

---
+++ src/main/java/com/gera/curso/springboot/di/factura/springbootdifactura/models/Invoice.java
@@ -0,0 +1,5 @@
+package com.gera.curso.springboot.di.factura.springbootdifactura.models;
+
+public class Invoice {
+
+}
---
+++ src/test/java/com/gera/curso/springboot/di/factura/springbootdifactura/SpringbootDiFacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gera.curso.springboot.di.factura.springbootdifactura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDiFacturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
